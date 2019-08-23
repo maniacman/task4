87d1e8526ff0735d655af52f2381bc91c19b8275
maniacman <32880757+maniacman@users.noreply.github.com>
2019-08-23 17:52:22 +0300

    Add files via upload

--- insert.php
+++ insert.php
@@ -6,6 +6,8 @@ $email = htmlspecialchars(trim($_POST['email']));
 $password = htmlspecialchars(trim($_POST['password']));
 $password_confirmation = htmlspecialchars(trim($_POST['password_confirmation']));
 
+$password = password_hash($password, PASSWORD_DEFAULT);
+
 $pdo = new PDO('mysql:host=localhost;dbname=blog;charset=utf8;', 'root', '');
 $statement = $pdo->prepare("INSERT INTO `users` (role, login, email, password, filename) VALUES(:role, :login, :email, :password, :filename)");
 $values = ['role' => 'user', 'login' => $login, 'email' => $email, 'password' => $password, 'filename' => 'user.jpg'];
